f919f6c593f6be087b3d88f18b137234aad1f29d
Gabriel Yoshikawa Koti <gabrielkoti@hotmail.com>
2023-01-16 13:26:47 -0300

    Commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# dia1
+# nodeExpressTS
  
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,31 @@
 import express from "express";
-
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import axios from "axios";
 
 dotenv.config()
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 4000;
+app.get('/', async function (req, res) {
+    try {
+        const response = await axios.get('https://api.weather.gov/gridpoints/TOP/31,80/forecast');
+        const test = response.data.properties.periods
+        
+        const filtered = test.map((element: { name: any; temperature: any; windSpeed: any; }) => {
+            return {
+                day: element.name,
+                temp: element.temperature,
+                wind: element.windSpeed
+            }
+        });
+        console.log(filtered);
 
-app.get("/", (req, res) => res.send("Hello from server!"));
+        res.json(filtered)
+    } catch (error) {
+        console.error(error);
+        res.status(500)
+    }
+  })
 
-app.listen(PORT, () => console.log(`⚡Server is running here 👉 https://localhost:${PORT}`));
+app.listen(PORT);
