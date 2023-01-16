086a5416627a01ee96b88be94bacf3bbc2d73fb6
Gabriel Yoshikawa Koti <gabrielkoti@hotmail.com>
2023-01-16 14:23:42 -0300

    Commit

--- src/server.ts
+++ src/server.ts
@@ -9,19 +9,28 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', async function (req, res) {
     try {
+        // Pega a API
         const response = await axios.get('https://api.weather.gov/gridpoints/TOP/31,80/forecast');
         const test = response.data.properties.periods
         
-        const filtered = test.map((element: { name: any; temperature: any; windSpeed: any; }) => {
+        // Filtro geral dos dados
+        const filtered = test.map((element: { number: any; name: any; temperature: any; windSpeed: any; }) => {
             return {
-                day: element.name,
+                id: element.number,
+                dayName: element.name,
                 temp: element.temperature,
                 wind: element.windSpeed
             }
         });
-        console.log(filtered);
 
+        // Filtro por dia
+         const result = filtered.filter((obj: { id: any; }) => {
+             return obj.id == '1';
+        });
+        console.log(filtered);
         res.json(filtered)
+
+        // Erro
     } catch (error) {
         console.error(error);
         res.status(500)
