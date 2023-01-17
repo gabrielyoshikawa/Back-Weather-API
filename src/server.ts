e241bff3124b121f88cc103fcba2b4e701ec2f84
Gabriel Yoshikawa Koti <gabrielkoti@hotmail.com>
2023-01-17 14:52:13 -0300

    Commit

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,10 @@ dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+let objData: any = {}
+
+let dias = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+
 app.get('/', async function (req, res) {
     try {
         // Pega a API
@@ -14,21 +18,56 @@ app.get('/', async function (req, res) {
         const test = response.data.properties.periods
         
         // Filtro geral dos dados
-        const filtered = test.map((element: { number: any; name: any; temperature: any; windSpeed: any; }) => {
+        const filtered = test.map((element: { number: any; name: any; startTime: any; temperature: any; windSpeed: any; }) => {
             return {
                 id: element.number,
                 dayName: element.name,
+                startTime: element.startTime,
                 temp: element.temperature,
                 wind: element.windSpeed
             }
         });
 
-        // Filtro por dia
-        const result = filtered.filter((obj: { id: any; }) => {
-            return obj.id == '1';
-        });
-        console.log(filtered);
-        res.json(filtered)
+        // Define o dia como chave
+        filtered.forEach((obj: {
+
+            tempMax: number; id: any; dayName: any; startTime: any; temp: any; wind: any; tempMin: number; diaSemana: string;  windMin: any; windMax: any;
+        }) => {
+
+            objData[obj.dayName] = obj
+            obj.diaSemana = dias[new Date(obj.startTime).getDay()]
+            obj.tempMax = obj.temp + 5
+            obj.tempMin = obj.temp - 5
+
+            var res = obj.wind.split(',').map((el: any) => {
+                let n = Number(el);
+                return n === 0 ? n : n || el;
+            });
+
+            console.log(res)
+            console.log(res[0][0])
+            // var splitted = obj.wind.split("")
+            // console.log(splitted)
+            // var splitted2 = splitted.split(",")
+            // console.log(splitted2)
+            // if (splitted.includes('t', 'o')){
+            //     obj.windMin = (splitted[0], splitted[1])
+                
+            //     // obj.windMax = (splitted[]) Fazer Vento Máximo
+            // } else {
+
+            // }
+
+        }); 
+
+
+        console.log(objData);
+        res.json(objData)
+
+        // Temperatura
+        // console.log(objData['Wednesday Night'].id)
+        // var tempMaxToday = objData.Tonight.temp = objData.Tonight.temp + 5
+        // console.log(tempMaxToday)
 
         // Erro
     } catch (error) {
